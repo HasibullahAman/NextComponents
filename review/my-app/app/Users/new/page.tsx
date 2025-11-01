af106c25969abd45a0bb373a745451e2f929ee95
HasibullahAman <hasib.aman7@gmail.com>
2025-11-01 12:04:13 +0430

    Routing and Link in nextjs

---
+++ review/my-app/app/Users/new/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewUsers = () => {
+    return (
+        <div>
+            <p>Hello this is the new Issue..</p>
+        </div>
+    )
+}
+
+export default NewUsers
---
+++ review/my-app/app/Users/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const usersPage = () => {
+    return (
+        <div>
+            <h1>Hell All this is the Users Page</h1>
+        </div>
+    )
+}
+
+export default usersPage
