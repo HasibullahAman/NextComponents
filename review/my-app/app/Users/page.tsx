d31c309b529b608dcfb596194f5b3da01b3b7ddb
HasibullahAman <hasib.aman7@gmail.com>
2025-11-10 11:58:42 +0430

    work on fetch data

--- review/my-app/app/Users/page.tsx
+++ review/my-app/app/Users/page.tsx
@@ -1,11 +1,48 @@
 import React from 'react'
 
-const usersPage = () => {
+interface User {
+    id: number;
+    name: string;
+}
+
+
+const UserPage = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const users: User[] = await res.json();
     return (
-        <div>
-            <h1>Hell All this is the Users Page</h1>
+        // <>
+        //     <h1>All Users</h1>
+        //     <ul>
+        //         {users.map(users => <li key={users.id}>{users.name}</li>)}
+        //     </ul>
+        // </>
+        <div className='p-6'>
+            <h1 className='text-xl font-bold mb-4'>Table of Users</h1>
+            <table className='min-w-full border border-gray-300'>
+                <thead className='bg-gray-100 text-black'>
+                    <tr>
+                        <th className='border px-4 py-2'>
+                            ID
+                        </th>
+                        <th className='border px-4 py-2'>
+                            Name
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map(users =>
+                        <tr key={users.id}>
+                            <td className='border px-4 py-2'>
+                                {users.id}
+                            </td>
+                            <td className='border px-4 py-2'>
+                                {users.name}
+                            </td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
         </div>
     )
 }
-
-export default usersPage
+export default UserPage
